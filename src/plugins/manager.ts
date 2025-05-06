52a4fdae0a2ac0189a1cf31bab322b35b8fd8b0e
Eth3rnit3 <eth3rnit3@gmail.com>
2025-05-07 00:09:21 +0200

    Update manager.ts

--- src/plugins/manager.ts
+++ src/plugins/manager.ts
@@ -1,13 +1,9 @@
 import { pluginLoaders } from './bundle'
-import plugin from './oss-default'
 import { C4Plugin, registry } from './registry'
 
-const loadOssDefault = () => plugin.setup(registry)
-
 export async function loadPlugins() {
-  const wanted = (import.meta.env.VITE_PLUGINS ?? '').split(',').filter(Boolean)
+  const wanted = import.meta.env.VITE_PLUGINS ? (import.meta.env.VITE_PLUGINS ?? '').split(',').filter(Boolean) : ['@archivisio/default']
 
-  loadOssDefault()
   await Promise.all(
     wanted.map(async (name: string) => {
       const loader = (pluginLoaders as Record<string, () => Promise<{ default: C4Plugin }>>)[name]
