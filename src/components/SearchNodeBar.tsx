d4b8e223e9ac8eeb92fd3ce06912ac69c7690f6b
Eth3rnit3 <eth3rnit3@gmail.com>
2025-05-07 21:13:07 +0200

    handle close

--- src/components/SearchNodeBar.tsx
+++ src/components/SearchNodeBar.tsx
@@ -1,22 +1,31 @@
 import { useDialogs } from "@/contexts/DialogContext";
 import { Box, TextField } from "@mui/material";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import useOnClickOutside from "@/hooks/useOnClickOutside";
 
 const SearchNodeBar: React.FC = () => {
   const [searchValue, setSearchValue] = useState<string>("");
-  const { pendingConnection } = useDialogs();
+  const { pendingConnection, setPendingConnection } = useDialogs();
+  const searchBarRef = useRef<HTMLDivElement | null>(null);
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(event.target.value);
   };
 
+  useOnClickOutside(searchBarRef, () => {
+    if (pendingConnection) {
+      setPendingConnection(null);
+    }
+  });
+
   if (!pendingConnection) return null;
   const { event, connectionState } = pendingConnection;
-
   console.log(event, connectionState);
 
   return (
-    <Box sx={{ position: "absolute", top: connectionState.to?.y, left: connectionState.to?.x, zIndex: 1, backgroundColor: "rgba(0,0,0,0.2)", backdropFilter: "blur(4px)" }}>
+    <Box 
+      ref={searchBarRef}
+      sx={{ position: "absolute", top: connectionState.to?.y, left: connectionState.to?.x, zIndex: 1, backgroundColor: "rgba(0,0,0,0.2)", backdropFilter: "blur(4px)" }}>
       <TextField
         label="Search"
         variant="outlined"
