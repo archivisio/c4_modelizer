34d833a334f226e66a54760ae7d0d2fe6b64e2c3
Eth3rnit3 <eth3rnit3@gmail.com>
2025-05-09 11:39:50 +0200

    feat: Add MUI theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,8 @@
+import { createTheme } from '@mui/material/styles';
+
+// Création d'un thème MUI par défaut
+const theme = createTheme({
+  // La configuration du thème sera ajoutée ici ultérieurement
+});
+
+export default theme;
