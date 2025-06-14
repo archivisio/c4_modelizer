53df8e0409eeb12a6e8dcb3c121e8a828180118b
Eth3rnit3 <eth3rnit3@gmail.com>
2025-06-14 12:05:00 +0200

    core(ServiceWorker): Enable updates notification

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,27 @@
-import { loadPlugins } from '@/plugins/manager'
-import PortalTarget from '@/slots/PortalTarget.tsx'
-import { DialogProvider } from '@contexts/DialogProvider.tsx'
-import { ThemeProvider } from '@mui/material/styles'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import RootProviderSlot from './RootProviderSlot.tsx'
-import theme from './theme/theme'
+import UpdateNotification from "@/components/UpdateNotification";
+import { loadPlugins } from "@/plugins/manager";
+import PortalTarget from "@/slots/PortalTarget.tsx";
+import { DialogProvider } from "@contexts/DialogProvider.tsx";
+import { ThemeProvider } from "@mui/material/styles";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import RootProviderSlot from "./RootProviderSlot.tsx";
+import theme from "./theme/theme";
 
 loadPlugins().then(() => {
-  createRoot(document.getElementById('root')!).render(
+  createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <ThemeProvider theme={theme}>
         <RootProviderSlot>
           <DialogProvider>
             <App />
+            <UpdateNotification />
           </DialogProvider>
         </RootProviderSlot>
         <PortalTarget id="global-overlay" />
       </ThemeProvider>
-    </StrictMode>,
-  )
+    </StrictMode>
+  );
 });
